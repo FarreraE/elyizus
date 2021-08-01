c420459839e677f586c84f09b8f7a0670a84f611
farrerae <farreraecontactos@gmail.com>
2021-08-01 21:56:44 +0200

    Section A content updated

--- js/main.js
+++ js/main.js
@@ -26,10 +26,10 @@ btnContact.addEventListener('click', function () {
     displayerAselector(displayerAOption);
 });
 btnNext.addEventListener('click', function () {
-    if(displayerAOption == 4){
+    if (displayerAOption == 4) {
         displayerAOption = 1;
-    }else{
-        displayerAOption ++;
+    } else {
+        displayerAOption++;
     }
     displayerAselector(displayerAOption);
 });
@@ -44,15 +44,16 @@ function displayerAselector(option) {
 
     switch (option) {
         case 1:
-            sectionADisplayer.innerHTML = "<span class=codeVSQoutes> <h2>ABOUT IT!</h2>  </span> <p>Elyizus is the first website created by Ely Jesus Farrera. The aim is to create a project of projects. A portfolio that will improve as I obtain knowledge and new skills. The most relevant characteristic of Elyizus personal project is to register development and personal growth. The first release is dated August 2021 when completing the first level of Fullstack dedicated to HTML and CSS. Check on the timeline to see how far I am from that point and what I have achieved. #Peace</p>";
+            sectionADisplayer.innerHTML = "<span class=codeVSQoutes> <h2>ABOUT IT!</h2>  </span> <p>Elyizus is the first project executed by Ely Jesus Farrera. The aim is to create a gallery of projects. A portfolio that will improve as I obtain knowledge and new skills. The most relevant characteristic of Elyizus personal project is to register development and personal growth. This website is the second version, the first version's catalog is available in the project section. The first release is dated July 2021 while studying the first level of Fullstack dedicated to HTML and CSS. Check on the timeline to see how far I am from that point and what I have achieved. <span class=codeVSQoutes>#Peace</span></p>";
             btnElyizus.style = "color: var(--colorVSQoutes);opacity: 1;"
             break;
         case 2:
-            sectionADisplayer.innerHTML = "<span class=codeVSTag> <h2>SHOW TIME!</h2> </span> <p>- Fullstack program : Coder House, In progress.<br>- Technical Computer Engineering: Seneca College, Canada 2016.<br>- Pathway Program (TOEFL): ILAC, Canada 2014-2015.<br>- Diploma in Sound Engineering: Audioplace, Venezuela 2014.<br> - Systems Engineering (2 years): Metropolitan University, Venezuela 2010</p>";
+            sectionADisplayer.innerHTML = "<span class=codeVSTag> <h2>ABOUT ME!</h2></span> <p>Computer technician with adaptability for teamwork, skills for research, analysis, and problem-solving. Knowledge of computer packages, programming languages, and basics of telematic networks. I aim to create or find a project to implement my knowledge assets and develop new skills.<br><span class=codeVSTag>TOOLS:</span> JS - HTML - CSS - VBA - Windows OS Office - Photoshop - Illustrator - VSC and more. </p>";
             btnSkills.style = "color: var(--colorVSTag);opacity: 1;"
             break;
+            
         case 3:
-            sectionADisplayer.innerHTML = "<span class=pComent> <h2>ABOUT ME!</h2> </span> <p>Computer technician with adaptability for teamwork, skills for research, analysis, and problem-solving. Knowledge of computer packages, programming languages, and basics of telematic networks. I aim to create or find a project that I could use my knowledge assets and develop new skills.<br>TOOLS: JS - HTML - CSS - VBA - Windows OS Office - Photoshop - Illustrator - VSC</p>";
+            sectionADisplayer.innerHTML = "<span class=pComent> <h2>THE BIG TIME!</h2></span> <p><span class=pComent>- Fullstack program :</span> Coder House, In progress.<span class=pComent><br>- Technical Computer Engineering (2 years):</span> Seneca College, Canada 2016.<span class=pComent> <br>- Pathway Program (TOEFL):</span> ILAC, Canada 2014-2015. <span class=pComent> <br>- Diploma in Sound Engineering:</span> Audioplace, Venezuela 2014. <span class=pComent>  <br> - Systems Engineering (2 years): </span> Metropolitan University, Venezuela 2010</p>";
             btnTime.style = "color: var(--colorVSComment); opacity: 1;"
             break;
         case 4:
